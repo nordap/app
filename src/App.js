31825131186269216a624b496b50415c61a98f27
KBS <nordap@naver.com>
2023-09-25 15:59:35 +0900

    fix locale

--- src/App.js
+++ src/App.js
@@ -224,12 +224,13 @@ function App() {
             let now = parseInt(arcainState.now);
             let need = n * n + 11;
             let remain = need - now;
+            const meso = Math.floor(need * 2 * ((regionConst + 4) + (n * 0.05))) * 10000
 
             //필요 성장치 x 2 x {(지역상수+4) + (레벨*0.05)}
             let result =
               "남은 갯수 " + remain + "개\n" +
               "남은 일수 " + ((remain - (remain % 20)) / 20 + (remain % 20 == 0 ? 0 : 1)) + "일\n" +
-              Math.floor(need * 2 * ((regionConst + 4) + (n * 0.05))) * 10000 + "메소"
+              meso.toLocaleString("en-US") + "메소"
               ;
 
             setArcain(result);
@@ -262,6 +263,7 @@ function App() {
             let now = parseInt(authenticState.now);
             let need = 9 * n * n + 20 * n;
             let remain = need - now;
+            const meso = Math.floor(need * 1.8 * ((regionConst + 7) - (n - 1) / 3)) * 10000;
 
             //필요 성장치 × 1.8 × {(지역상수 + 7) - (레벨 - 1)÷3}
             let result =
@@ -269,7 +271,7 @@ function App() {
               "남은 일수 " + (regionConst == 0
                 ? ((remain - (remain % 20)) / 20 + (remain % 20 == 0 ? 0 : 1))
                 : ((remain - (remain % 10)) / 10 + (remain % 10 == 0 ? 0 : 1))) + "일\n" +
-              Math.floor(need * 1.8 * ((regionConst + 7) - (n - 1) / 3)) * 10000 + "메소"
+              meso.toLocaleString("en-US") + "메소"
               ;
 
             setAuthentic(result);
